3ca4b80c27c5552bcdb2e45b055f92e044ff9e89
Chamuditha Wasala <chamudithaprabathwasala@gmail.com>
2023-10-15 13:22:57 +0530

    Add styles for Resturant screen header section

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -5,6 +5,8 @@ import tw from "twrnc";
 import { urlFor } from "../sanity";
 import { StatusBar } from "expo-status-bar";
 import { ArrowLeftIcon } from "react-native-heroicons/outline";
+import { StarIcon } from "react-native-heroicons/solid";
+import { MapPinIcon } from "react-native-heroicons/outline";
 
 const RestaurantScreen = () => {
   const navigation = useNavigation();
@@ -36,6 +38,23 @@ const RestaurantScreen = () => {
         >
           <ArrowLeftIcon size={20} color="#00CCBB" />
         </TouchableOpacity>
+      </View>
+      <View style={tw`bg-white`}>
+        <View style={tw`px-4 pt-4`}>
+          <Text style={tw`text-3xl font-bold`}>{title}</Text>
+          <View style={tw`flex-row my-1`}>
+            <View style={tw`flex-row items-center`}>
+              <StarIcon color="green" opacity={0.5} size={20} />
+              <Text style={tw`mx-2 text-green-500 text-xs`}>{raiting} </Text>
+              <Text style={tw`text-xs text-gray-500`}>{genre}</Text>
+            </View>
+            <View style={tw`flex-row items-center mx-2`}>
+              <MapPinIcon color="gray" opacity={0.4} size={20} />
+              <Text style={tw`text-xs text-gray-500`}>Nearby {address}</Text>
+            </View>
+          </View>
+          <Text style={tw`text-gray-500 mt-2 pb-2`}>{short_description}</Text>
+        </View>
       </View>
       <StatusBar style="auto" />
     </ScrollView>
