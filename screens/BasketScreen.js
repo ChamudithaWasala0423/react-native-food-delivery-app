49bdd97ff75f73fe46daf09d82b95b80eef250b5
Chamuditha Wasala <chamudithaprabathwasala@gmail.com>
2023-10-22 00:03:22 +0530

    Change UseMemo to useEffect

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,5 +1,5 @@
 import { View, Text } from "react-native";
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import tw from "twrnc";
 import { useSelector, useDispatch } from "react-redux";
@@ -13,7 +13,7 @@ const BasketScreen = () => {
   const dispatch = useDispatch();
   const [groupBasketItems, setGroupBasketItems] = useState([]);
 
-  useMemo(() => {
+  useEffect(() => {
     const groupItems = items.reduce((result, item) => {
       (result[item.id] = result[item.id] || []).push(item);
       return result;
